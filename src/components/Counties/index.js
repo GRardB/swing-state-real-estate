3f01ad97eac3f6980c97d85a55460ed7714a04f1
OnuPete <pcr137@case.edu>
2017-04-29 15:24:43 -0700

    County button design

--- src/components/Counties/index.js
+++ src/components/Counties/index.js
@@ -7,8 +7,8 @@ const Counties = ({ counties }) => (
       counties.map(county => (
         <button key={county.name} className={styles.display}>
           <h2>{county.name}</h2>
-          <img src="democrat.png" alt="Democrats" title="Democrats" className={styles.percent_img} />{county.dems}%
-          <img src="republican.png" alt="Republicans" title="Republicans" className={styles.percent_img} />{county.reps}%
+          <img src="democrat.png" alt="Democrats" title="Democrats" className={styles.percent_img} /><em>{county.dems}%</em>&nbsp; &nbsp;
+          <img src="republican.png" alt="Republicans" title="Republicans" className={styles.percent_img} /><em>{county.reps}%</em>
         </button>
       ))
     }
