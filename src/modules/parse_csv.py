d0d1c2507b419456844cae6156442f26aee7e83e
OnuPete <pcr137@case.edu>
2017-04-29 05:30:18 -0700

    Add county level information
    Created a Json type object from a csv file of county level election
    percentages from 2016

---
+++ src/modules/parse_csv.py
@@ -0,0 +1,30 @@
+import csv
+
+def parse_csv():
+
+    """
+    for every state, make a dict containing a list of all counties
+    for every county in state, include county_name, percentage of dem and rep
+
+    {'state_abbr':
+        [{'per_dem': per_dem,
+        'per_gop': per_gop,
+        'county_name': county_name}, ...],
+     'state_abbr':...
+    }
+    """
+
+    json_file = {}
+
+    with open('2016_US_County_Level_Presidential_Results.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data = {'per_dem': row['per_dem'],
+                    'per_gop': row['per_gop'],
+                    'county_name': row['county_name']}
+            if row['state_abbr'] in json_file:
+                json_file[row['state_abbr']].append(data)
+            else:
+                json_file[row['state_abbr']] = [data]
+
+    return json_file
